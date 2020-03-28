4984ed1ad048b142955f1a5359b0a04fe2aa444a
Lucas Machado <lucas.machado290@gmail.com>
2020-03-28 16:23:11 -0300

    Initial commit

---
+++ src/test/java/com/example/springbootangular/SpringBootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
